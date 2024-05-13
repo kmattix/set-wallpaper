5f9b156331192f82cc328dad1b927cc3ed22e920
Kevin Mattix <kmattrider@gmail.com>
2024-05-12 23:12:08 -0400

    added file sanitization, and moved logic into functions

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/kmattix/set-wallpaper
 
 go 1.22.2
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
--- set-wallpaper.go
+++ set-wallpaper.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/gabriel-vasile/mimetype"
 )
 
 func main() {
-	args := os.Args
+	args := sanitizeArgLength(1, 1)
+	sanitizeFile(args[0], "image/jpeg", "image/png")
+	prepareConfigFolder("wallpaper")
+}
 
-	if len(args) < 2 {
+// Verify that the program execution arguments are not less than min, and not greater than max.
+// Not including the program name 0th argument. This will fatally kill the program if the arguments provided
+// do not fit the parameter requirements. Otherwise it will return the sanitized args.
+func sanitizeArgLength(min, max int) []string {
+	args := os.Args
+	if len(args) < min+1 {
 		log.Fatal("No arguments provided.")
-	} else if len(args) > 2 {
+	} else if len(args) > max+1 {
 		log.Fatal("Too many arguments provided.")
 	}
+	return args[1:]
+}
+
+// Create a config folder of a specified name if one does not exist already.
+func prepareConfigFolder(folderName string) {
+	if ucd, err := os.UserConfigDir(); err == nil {
+		dir := fmt.Sprintf("%s/%s", ucd, folderName)
+		os.Mkdir(dir, os.FileMode(0755))
+	}
+}
+
+// Will kill the program fatally if the file does not exist or if it does not fit into one of the specified mimes.
+func sanitizeFile(path string, fileMimes ...string) {
+	mt, err := mimetype.DetectFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, fm := range fileMimes {
+		if mt.Is(fm) {
+			return
+		}
+	}
+	log.Fatalf("%s is a non-valid file type: %s", path, mt.String())
 }
